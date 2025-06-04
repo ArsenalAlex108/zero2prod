be2bcb1ca29646ee11fab4d007996e28cf1dc078
ArsenalAlex108 <107184385+ArsenalAlex108@users.noreply.github.com>
2025-06-04 16:02:39 +0700

    Chapter 3 completed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to find configuration file.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    .await
+    .expect("");
     TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))
-    .and_then(startup::run)
+    .and_then(|i| startup::run(i, connection_pool))
     ?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,11 +1,37 @@
-use actix_web::{web::Form, HttpResponse, Responder};
+use actix_web::{
+    web::{self}, HttpResponse, Responder
+};
+use sqlx::{PgPool};
+use chrono::Utc;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct SubscribeFormData {
     pub email: String,
-    pub name: String
+    pub name: String,
 }
 
-pub async fn subscribe(form: Form<SubscribeFormData>) -> impl Responder {
-    HttpResponse::Ok()
+pub async fn subscribe(
+    form: web::Form<SubscribeFormData>,
+    pool: web::Data<PgPool>,
+) -> impl Responder {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await
+    .map_or_else(
+        |e| {
+            println!("Query error: {e}");
+            HttpResponse::InternalServerError().finish()
+        }, 
+        |_|HttpResponse::Ok().finish()
+    )
 }
